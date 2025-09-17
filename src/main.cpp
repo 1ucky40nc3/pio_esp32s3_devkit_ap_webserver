1eac2c064f17870c8fb33617f4cbd7e090728a64
1ucky40nc3 <louisnwendler@gmail.com>
2025-09-17 19:51:14 +0200

    create wifi ap

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <led.h>
 #include <config.h>
+#include <secrets.h>
 
 void setup()
 {
   Serial.begin(SERIAL_BAUD);
   delay(DELAY_AFTER_SERIAL_INITIALIZED); // Give time for serial monitor to connect
+
+  // Set the Wi-Fi mode to Access Point
+  WiFi.mode(WIFI_AP);
+
+  // Start the AP with the specified SSID and password
+  WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+
+  // Print the details
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
+  Serial.print("AP SSID: ");
+  Serial.println(WIFI_SSID);
 }
 
 void loop()
