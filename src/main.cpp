be4099e5112d0cfc182441ebd4b266e8809e6a68
1ucky40nc3 <louisnwendler@gmail.com>
2025-09-19 17:23:30 +0200

    successfully host build of svelte_fingi

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,66 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <ArduinoJson.h>
-#include <SPIFFS.h>
+#include <LittleFS.h> // Changed from <SPIFFS.h>
 #include <led.h>
 #include <config.h>
 #include <secrets.h>
 
 WebServer server(80);
 
-void handleRoot()
+// A new function to handle all static file requests, including the root URL.
+void handleStaticFile()
 {
-  server.send(200, "text/html", "<h1>Hello from your ESP32!</h1>");
-  Serial.println("Handle root");
+  String path = server.uri();
+
+  // If the path is the root URL "/", serve index.html
+  if (path.endsWith("/"))
+  {
+    path += "index.html";
+  }
+
+  String dataType = "text/plain";
+  if (path.endsWith(".html"))
+  {
+    dataType = "text/html";
+  }
+  else if (path.endsWith(".js"))
+  {
+    dataType = "application/javascript";
+  }
+  else if (path.endsWith(".css"))
+  {
+    dataType = "text/css";
+  }
+  else if (path.endsWith(".json"))
+  {
+    dataType = "application/json";
+  }
+  // Add more file types as needed
+
+  // Check if the requested file exists in LittleFS
+  if (LittleFS.exists(path))
+  {
+    File file = LittleFS.open(path, "r");
+    server.streamFile(file, dataType);
+    file.close();
+  }
+  else
+  {
+    // If the file does not exist, serve index.html as a fallback.
+    // This is crucial for single-page applications like SvelteKit.
+    File file = LittleFS.open("/index.html", "r");
+    if (file)
+    {
+      server.streamFile(file, "text/html");
+      file.close();
+    }
+    else
+    {
+      // If even index.html doesn't exist, send a 404 Not Found error.
+      server.send(404, "text/plain", "404: Not Found");
+    }
+  }
 }
 
 void handleLedGreenGet()
@@ -35,7 +84,6 @@ void handleLedOffGet()
   String jsonOutput;
   serializeJson(doc, jsonOutput);
   turnLEDOff(LED_PIN);
-  server.send(200, "application/json", jsonOutput);
   Serial.println("Handle LED off GET");
 }
 
@@ -44,12 +92,24 @@ void setup()
   Serial.begin(SERIAL_BAUD);
   delay(DELAY_AFTER_SERIAL_INITIALIZED); // Give time for serial monitor to connect
 
-  // Initialize SPIFFS
-  if (!SPIFFS.begin(true))
+  // Initialize LittleFS, and format if mount fails.
+  if (!LittleFS.begin())
   {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
+    Serial.println("Mounting LittleFS failed. Attempting to format...");
+    if (LittleFS.format())
+    {
+      Serial.println("LittleFS formatted successfully. Rebooting to remount.");
+      delay(1000);
+      ESP.restart();
+    }
+    else
+    {
+      Serial.println("LittleFS format failed! An error has occurred.");
+      return;
+    }
   }
+
+  Serial.println("LittleFS mounted successfully.");
 
   // Set the Wi-Fi mode to Access Point
   WiFi.mode(WIFI_AP);
@@ -63,14 +123,13 @@ void setup()
   Serial.print("AP SSID: ");
   Serial.println(WIFI_SSID);
 
-  server.on("/", handleRoot); // Route for the root URL
   server.on("/led/green", HTTP_GET, handleLedGreenGet);
   server.on("/led/off", HTTP_GET, handleLedOffGet);
 
-  // Serve static files automatically from the root directory of SPIFFS
-  // The first argument is the URL path, the second is the file system path.
-  // The third argument (optional) is a cache control header.
-  server.serveStatic("/static/", SPIFFS, "/");
+  // Use a catch-all handler for any URI that is not specifically defined.
+  // This is where all the static files will be served from.
+  server.onNotFound(handleStaticFile);
+
   server.begin(); // Start the server
   Serial.println("HTTP server started");
 }
